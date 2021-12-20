da29fdfd483dce31a1e0de33a61e8475314a1238
Roy Babbel <roybabbel91@gmail.com>
2021-12-20 08:30:49 -0700

    Day 13 solutions

---
+++ 2021/13/part_2/solution.rb
@@ -0,0 +1,36 @@
+coordinates = {}
+folds = []
+
+File.foreach("../coordinates_data.txt", chomp: true) do |line|
+  coordinates[line] = true
+end
+
+File.foreach("../folds_data.txt", chomp: true) do |line|
+  axis, value = line.split(" ").last.split("=")
+  folds << [axis, value.to_i]
+end
+
+folds.each do |axis, fold_value|
+  idx = axis == "x" ? 0 : 1
+  max = coordinates.keys.map { |c| c.split(",")[idx].to_i }.max
+  offset = [max - 2 * fold_value, 0].max
+
+  coordinates = coordinates.keys.each_with_object({}) do |coords, transposed|
+    coords = coords.split(",").map(&:to_i)
+    if coords[idx] <= fold_value
+      coords[idx] = coords[idx] + offset
+    elsif coords[idx] <= 2 * fold_value
+      coords[idx] = (2 * fold_value - coords[idx]) + offset
+    else
+      coords[idx] = y_offset - (coords[idx] - 2 * fold_value)
+    end
+    transposed[coords.join(",")] = true
+  end
+end
+
+(0..coordinates.keys.map { |k| k.split(",").last.to_i }.max).each do |y|
+  (0..coordinates.keys.map { |k| k.split(",").first.to_i }.max).each do |x|
+    print coordinates["#{x},#{y}"] ? '#' : '.'
+  end
+  puts
+end
